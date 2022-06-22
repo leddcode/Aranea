188cc9b4c846912f1edbe7e5c52d3b1660c83706
leddest <46251307+leddest@users.noreply.github.com>
2022-06-22 18:20:04 +0300

    Fix coloring for Windows

--- mixins/colour.py
+++ mixins/colour.py
@@ -1,3 +1,9 @@
+import sys
+
+import win_unicode_console
+import colorama
+
+
 class Colour:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -10,3 +16,20 @@ class Colour:
     UNDERLINE = '\033[4m'
     WHITE = '\033[0m'
     ORANGE = '\033[33m'
+
+    if sys.platform.startswith('win'):
+        try:
+            win_unicode_console.enable()
+            colorama.init()
+        except Exception:
+            PURPLE = ''
+            CYAN = ''
+            DARKCYAN = ''
+            BLUE = ''
+            GREEN = ''
+            YELLOW = ''
+            RED = ''
+            BOLD = ''
+            UNDERLINE = ''
+            WHITE = ''
+            ORANGE = ''
