e7928dfbd48d2798b9d987e9fb4e0490f92b55a9
leddcode <leddest@gmail.com>
2026-01-18 17:48:50 +0200

    Improve email regex in crawler

--- plugins/crawler.py
+++ plugins/crawler.py
@@ -28,7 +28,7 @@ class Crawler:
     }
 
     URL_REG = r'http[s]?:[\\]?/[\\]?/(?:(?!http[s]?:[\\]?/[\\]?/)[a-zA-Z]|[0-9]|[\\]?[$\-_@.&+/]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    EMAIL_REG = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
+    EMAIL_REG = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 
     LOCK = Lock()
     LOGGED_URLS = {}
